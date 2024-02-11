17b549c620b2d0afa705822f20b2abc9e19253d2
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-02-11 21:29:40 +0530

    components made

---
+++ src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Counter = () => {
+    
+    
+    return (
+        <>
+            <button>Increment</button>
+            <button>Decrement</button>
+        </>
+    )
+}
+
+export default Counter;
